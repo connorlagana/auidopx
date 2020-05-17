72a2172177a14fc49bee74b153fa888f42b3095a
Connor Lagana <connor.lagana@gmail.com>
2020-05-17 19:16:48 -0400

    printing data

--- audioopx.py
+++ audioopx.py
@@ -10,7 +10,13 @@ wav = wave.open(SUMMER, 'r')
 
 print(audioop.avg(wav.readframes(wav.getnframes()), wav.getsampwidth()))
 
-wav.rewind() 
-print(audioop.max(wav.readframes(wav.getnframes()), wav.getsampwidth()))
+wav.rewind()
+
+print('Number of audio channels: ', wav.getnchannels())
+print('Sample width in bytes: ', wav.getsampwidth())
+print('Sampling Frequency: ', wav.getframerate())
+print('Number of audio frames: ', wav.getnframes())
+print('Compression type: ', wav.getcomptype())
+print('Compression name: ', wav.getcompname())
 
 #playsound(SUMMER)
